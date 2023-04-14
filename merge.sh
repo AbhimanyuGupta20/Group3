83c2ba3a1c135e6a05432f6396f1131e855d58f7
Peter Harmer <pharmer@wisc.edu>
2023-04-14 12:29:40 -0500

    merge.sh completed

--- merge.sh
+++ merge.sh
@@ -4,7 +4,7 @@
 #Merge the sorted small files into one large sorted file
 #Convert the merged sorted file to one called countsOfWords
 export LC_ALL=C
-sort -m plays_* | uniq -c >countsOfWords
+sort -m plays_* | uniq -c | sort -r >countsOfWords
 
 
 #File Cleanup
