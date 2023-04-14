278938fb1a0c0dbb2204561051b0928aa9174beb
Peter Harmer <pharmer@wisc.edu>
2023-04-14 11:01:41 -0500

    Removed + merged duplicate file PRE.sh with combine.sh

--- combine.sh
+++ combine.sh
@@ -3,4 +3,6 @@
 
 tar -xvf shakespeare.tar
 
-find shakespeare/* -maxdepth 4 -not -name 'README' -exec cat {} +>plays
+find shakespeare/* -maxdepth 4 -not -name 'README' -exec cat {} +>plays.txt
+
+split -n l/5 plays.txt plays_
