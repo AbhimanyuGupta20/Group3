e4409f13b9676ffb7616330e5fa1041fc374fce2
pharmer <pharmer@learn.chtc.wisc.edu>
2023-04-14 22:16:35 -0500

    fixed the dag file

--- combine.sh
+++ combine.sh
@@ -2,7 +2,6 @@
 
 wget https://pages.stat.wisc.edu/~jgillett/DSCP/CHTC/wordCounting/shakespeare.tar
 
-
 tar -xvf shakespeare.tar
 
 find shakespeare/* -maxdepth 4 -not -name 'README' -exec cat {} +>plays.txt
